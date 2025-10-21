bf3aedbf2af467fdeabbd3a71279120467c44987
Iver Mortensen <iver.mortensen@gmail.com>
2025-10-21 18:33:53 +0200

    Updated main.py to use new stablilization functions.

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import hashlib
 import logging as log
 import sys
+import random
 from http.server import ThreadingHTTPServer
 from time import sleep
 from threading import Thread
@@ -10,13 +11,12 @@ from log import init_logger
 from http_handler import create_handler
 
 
-def start_http(port: int, node: ChordNode):
-    handler = create_handler(node)
-    http_server = ThreadingHTTPServer(("0.0.0.0", port), handler)
-
-    server_thread = Thread(target=http_server.serve_forever)
-    server_thread.daemon = True
-    server_thread.start()
+def run_periodic_function(func, min_delay=5, max_delay=10):
+    """Run a function periodically with random delays"""
+    while True:
+        delay = random.uniform(min_delay, max_delay)
+        sleep(delay)
+        func()
 
 
 def main():
@@ -35,15 +35,27 @@ def main():
     node = ChordNode(ip=ip, port=port, id=id, m=m)
     log.info(f"Node initialized: id:{id}, m:{m}")
 
-    # Start HTTP server
-    try:
-        start_http(port, node)
-        log.info(f"HTTP server started: {ip}:{port}")
-    except Exception as e:
-        log.error(f"Failed to start HTTP server for {endpoint}: {e}")
+    # Schedule periodic functions
+    stabilize = Thread(
+        target=run_periodic_function, args=(node.stabilize,), daemon=True
+    )
+    # fix_fingers = Thread(
+    #     target=run_periodic_function, args=(node.fix_fingers,), daemon=True
+    # )
+    check_predecessor = Thread(
+        target=run_periodic_function, args=(node.check_predecessor,), daemon=True
+    )
 
-    while True:
-        sleep(1)
+    stabilize.start()
+    # fix_fingers.start()
+    check_predecessor.start()
+
+    # Start HTTP server
+    handler = create_handler(node)
+    http_server = ThreadingHTTPServer(("0.0.0.0", port), handler)
+    log.info(f"HTTP server started: {ip}:{port}")
+
+    http_server.serve_forever()
 
 
 if __name__ == "__main__":
