8a098fe967a2190fae4cd40d8f635c9aa5c45d60
Iver Mortensen <iver.mortensen@gmail.com>
2025-10-21 18:33:04 +0200

    Added endpoints for using chord stabilization.

--- src/http_handler.py
+++ src/http_handler.py
@@ -1,9 +1,9 @@
 import json
 from http.server import BaseHTTPRequestHandler
 import logging as log
-import requests
 
 from chord_node import ChordNode
+import chord_client
 
 
 class HTTPHandler(BaseHTTPRequestHandler):
@@ -16,7 +16,13 @@ class HTTPHandler(BaseHTTPRequestHandler):
         Handles get requests.
         """
         # Check the path and run the associated function
-        if self.path.startswith("/storage/"):
+        if self.path == ("/status"):
+            self.get_status()
+
+        elif self.path == ("/predecessor"):
+            self.get_predecessor()
+
+        elif self.path.startswith("/storage/"):
             try:
                 key = self.path.split("/storage/")[1]
             except (ValueError, IndexError):
@@ -31,10 +37,10 @@ class HTTPHandler(BaseHTTPRequestHandler):
             except (ValueError, IndexError):
                 self.send_error(400, "Invalid key format")
 
-        elif self.path.startswith("/successor/"):
+        elif self.path.startswith("/find_successor/"):
             try:
-                key = self.path.split("/successor/")[1]
-                self.get_successor(int(key))
+                key = self.path.split("/find_successor/")[1]
+                self.get_find_successor(int(key))
             except (ValueError, IndexError):
                 self.send_error(400, "Invalid key format")
 
@@ -64,6 +70,9 @@ class HTTPHandler(BaseHTTPRequestHandler):
             except (ValueError, IndexError):
                 self.send_error(400, "Invalid key format")
 
+        elif self.path == "/notify":
+            self.put_notify()
+
         elif self.path == "/fix_fingers":
             self.put_fix_fingers()
 
@@ -73,6 +82,22 @@ class HTTPHandler(BaseHTTPRequestHandler):
         # Unknown paths receive a 404
         else:
             self.send_error(404, "Not Found")
+
+    def get_status(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+
+    def get_predecessor(self):
+        predecessor = self.node.predecessor
+        if predecessor is None:
+            self.send_error(404, f"{self.node.id} does not have a predecessor")
+            return
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+        self.wfile.write(predecessor.encode("utf-8"))
 
     def get_value(self, key: str):
         value = self.node.get_value(key)
@@ -85,7 +110,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(value.encode("utf-8"))
 
-    def get_successor(self, key: int):
+    def get_find_successor(self, key: int):
         # Find successor
         successor = self.node.find_successor(key)
         if not successor:
@@ -116,7 +141,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
             return
 
         # Get the value
-        response = requests.get(f"http://{successor}/value/{raw_key}")
+        response = chord_client.get_value(successor, raw_key)
         if response.status_code != 200 or response.text is None:
             self.send_error(response.status_code, response.text)
             return
@@ -147,6 +172,26 @@ class HTTPHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Type", "application/json")
         self.end_headers()
         self.wfile.write(json.dumps(list(neighbours)).encode())
+
+    def put_notify(self):
+        content_length = int(self.headers.get("Content-Length", 0))
+        if not content_length:
+            self.send_error(400, "Empty request body")
+            return
+
+        # Get the predecessor from the body
+        body = self.rfile.read(content_length)
+        try:
+            predecessor = body.decode("utf-8").strip()
+        except UnicodeDecodeError:
+            self.send_error(400, "Invalid UTF-8 encoding")
+            return
+
+        self.node.notify(predecessor)
+
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
 
     def put_value(self, key: str):
         content_length = int(self.headers.get("Content-Length", 0))
@@ -198,7 +243,7 @@ class HTTPHandler(BaseHTTPRequestHandler):
             return
 
         # Insert value
-        response = requests.put(f"http://{successor}/value/{raw_key}", data=value)
+        response = chord_client.set_value(successor, raw_key, value)
         if response.status_code != 200:
             self.send_error(response.status_code, response.text)
 
