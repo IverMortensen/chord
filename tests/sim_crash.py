25ab402819a4b10505e2013026c954bfec91eb85
Iver Mortensen <iver.mortensen@gmail.com>
2025-10-27 18:56:35 +0100

    Updated sim_crash.py

--- tests/sim_crash.py
+++ tests/sim_crash.py
@@ -4,24 +4,33 @@ from utils import sim_crash, sim_recover, stabilize
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Usage: <num_nodes_to_crash> [host:port ...]")
+        print("Usage: [host:port ...]")
         sys.exit(1)
-    num_nodes = int(sys.argv[1])
-    host_ports = sys.argv[2:]
-
-    if num_nodes >= len(host_ports):
-        print(f"Num nodes to crash {num_nodes} >= nodes in ring {len(host_ports)}.")
-        exit(1)
-
-    print(f"Crashing {num_nodes} node(s)...")
+    host_ports = sys.argv[1:]
+    num_nodes = len(host_ports) - 1
     crashed_nodes = []
-    for _ in range(num_nodes):
-        node = host_ports.pop(0)
-        sim_crash(node)
-        crashed_nodes.append(node)
+    max = 0
 
-    print("Waiting for nodes to stabilize...")
-    stabilize(host_ports)
+    for i in range(1, num_nodes):
+        print(f"Crashing {i} node(s)...")
+        for j in range(i):
+            node = host_ports.pop(0)
+            sim_crash(node)
+            crashed_nodes.append(node)
+
+        print("Waiting for nodes to stabilize...")
+        res = stabilize(host_ports, 120)
+        if not res:
+            print("")
+            break
+        max = i
+
+        print(f"Recovering {i} node(s)...")
+        for j in range(i):
+            node = crashed_nodes.pop(0)
+            sim_recover(node)
+            host_ports.append(node)
+        print("")
 
     print(f"Recovering {num_nodes} node(s)...")
     for node in crashed_nodes:
@@ -29,4 +38,7 @@ if __name__ == "__main__":
         host_ports.append(node)
 
     print("Waiting for nodes to stabilize...")
-    stabilize(host_ports)
+    if not stabilize(host_ports):
+        print("Timelimit reached. Chord ring is still not stable.")
+
+    print(f"\nNumber of crashed nodes chord ring handled: {max}")
